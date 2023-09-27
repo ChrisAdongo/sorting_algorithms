0c77104a4f361968b88eba7b4ba12365daec8f4c
Chris Adongo <119563006+ChrisAdongo@users.noreply.github.com>
2023-09-27 08:09:25 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -34,7 +34,7 @@ void merge_sort(int *array, size_t size);
 void heapify(int *array, size_t size, int i);
 void heap_sort(int *array, size_t size);
 int get_max(int *array, size_t size);
-void counting_sort(int *array, size_t size, int exp);
+void counting_sorter(int *array, size_t size, int exp);
 void radix_sort(int *array, size_t size);
 void bitonic_compare(int *array, size_t size, int dir);
 void bitonic_merge(int *array, size_t size, int dir);
